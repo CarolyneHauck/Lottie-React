a8bf1f2ad3ea1717eccf5bda09c1dd02d6ae0154
CarolyneHauck <carolyne.carreira@gmail.com>
2020-11-01 23:09:49 -0300

    add animation

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import LottieView from 'lottie-react-native';
+import Title from './images/blogger.json';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class BasicExample extends React.Component {
+  componentDidMount() {
+    this.animation.play();
+    // Or set a specific startFrame and endFrame with:
+    this.animation.play(30, 120);
+  }
+
+  render() {
+    return (
+      <LottieView
+        ref={animation => {
+          this.animation = animation;
+        }}
+        source={Title}
+      />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
